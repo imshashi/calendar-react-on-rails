10e5595fa93a2e78364f55f06dcf3e773acd02b0
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-09-21 20:28:22 +0530

    use react-router

---
+++ app/javascript/bundles/Appointments/components/AppRouter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BroswerRouter as Router, Route } from 'react-router-dom';
+
+export default (props) => {
+  return (
+    <Router>
+      <Route path="/" />
+    </Router>
+  )
+}
